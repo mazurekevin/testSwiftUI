d73a40f9e080303d1ce97f04a706ad662916ce22
kmazure <kmazure@teamdoc.fr>
2024-01-11 15:55:14 +0100

    Initial Commit

---
+++ testSwiftUI/testSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftUIApp.swift
+//  testSwiftUI
+//
+//  Created by teamdoc on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
