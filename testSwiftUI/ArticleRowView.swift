2d6d8593842bc6daa9508c8c57df4968ad31ad29
kmazure <kmazure@teamdoc.fr>
2024-05-29 21:18:28 +0200

    cell article+appBar

---
+++ testSwiftUI/ArticleRowView.swift
@@ -0,0 +1,8 @@
+//
+//  ArticleRowView.swift
+//  testSwiftUI
+//
+//  Created by teamdoc on 29/05/2024.
+//
+
+import Foundation
---
+++ testSwiftUI/HeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  HeaderView.swift
+//  testSwiftUI
+//
+//  Created by teamdoc on 29/05/2024.
+//
+
+import Foundation
